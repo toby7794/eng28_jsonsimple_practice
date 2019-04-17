e0def94f3b65fd559a6f5dda98acffebee7f5b42
T L Goddard <TGoddard@spartaglobal.com>
2019-04-17 14:39:36 +0100

    Second Commit

---
+++ src/main/java/com/sparta/tlg/App.java
@@ -0,0 +1,13 @@
+package com.sparta.tlg;
+
+import org.json.simple.JSONObject;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        JSONObject userDetails = new JSONObject();
+
+        System.out.println(userDetails.getClass());
+    }
+}
